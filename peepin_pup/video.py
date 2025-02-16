5d04891434a01db8e6f99a0d77e62533f36024e3
William Radhuber <48190729+WCRadhuber@users.noreply.github.com>
2025-02-15 19:42:23 -0500

    Update video.py
    Improved Stream. Need to add graceful stop.

--- peepin_pup/video.py
+++ peepin_pup/video.py
@@ -4,7 +4,7 @@ from threading import Condition
 from flask import Blueprint, render_template, stream_template, Response, session
 from peepin_pup.auth import login_required
 from picamera2 import Picamera2
-from picamera2.encoders import JpegEncoder
+from picamera2.encoders import MJPEGEncoder, Quality
 from picamera2.outputs import FileOutput
 bp = Blueprint('video', __name__)
 
@@ -12,13 +12,12 @@ logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(message)s", date
 logger = logging.getLogger(__name__)
 
 picam2 = Picamera2()
-camera_config = picam2.create_preview_configuration()
-picam2.configure(camera_config)
+video_config = picam2.create_video_configuration(main={"size": (1920, 1080)})
+picam2.configure(video_config)
 
 class StreamingOutput(io.BufferedIOBase):
     def __init__(self):
         self.frame = None
-        self.buffer = io.BytesIO()
         self.condition = Condition()
 
     def write(self, buf):
@@ -33,9 +32,6 @@ def gen_frames(output):
             with output.condition:
                 output.condition.wait()
                 frame = output.frame
-                if frame is None:
-                    logger.warning("Empty frame recieved")
-                    continue
             yield (b'--frame\r\n'
                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
     except Exception as e:
@@ -46,16 +42,3 @@ def gen_frames(output):
 def video_feed():
     username = session.get("username")
     output = StreamingOutput()
-    try:
-        picam2.start_recording(JpegEncoder(), FileOutput(output))
-        logger.info(f"{username} Started video feed")
-        return Response(gen_frames(output),
-                       mimetype='multipart/x-mixed-replace; boundary=frame')
-    except Exception as e:
-        logger.error(f"Error starting video feed {str(e)}")
-        return Response("Error starting video feed", status=500)
-
-@bp.route('/')
-@login_required
-def index():
-    return stream_template('video/index.html')
